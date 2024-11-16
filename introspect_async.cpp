200ea5c63d802fbfc59067df35df85d3d1219ec0
Kevin Backhouse <kevinbackhouse@github.com>
2024-11-16 22:44:19 +0000

    Update submodules to latest versions

--- introspect_async.cpp
+++ introspect_async.cpp
@@ -15,81 +15,64 @@
 // You should have received a copy of the GNU General Public License
 // along with DBusParseDemo.  If not, see <https://www.gnu.org/licenses/>.
 
-
+#include <EPollLoopDBusHandler.hpp>
+#include <dbus_utils.hpp>
 #include <string.h>
 #include <unistd.h>
-#include <dbus_utils.hpp>
-#include <EPollLoopDBusHandler.hpp>
 
 class IntrospectHandler : public DBusHandler {
   const std::string destination_;
   const std::string objectpath_;
 
 public:
-  IntrospectHandler(
-    const char* socketpath, const char* destination, const char* objectpath
-  ) :
-    DBusHandler(socketpath),
-    destination_(destination),
-    objectpath_(objectpath)
-  {}
+  IntrospectHandler(const char *socketpath, const char *destination,
+                    const char *objectpath)
+      : DBusHandler(socketpath), destination_(destination),
+        objectpath_(objectpath) {}
 
   void accept() override final {
-    send_hello(
-      [this](const std::string& busname) -> int {
-        fprintf(stdout, "Unique bus name: %s\n", busname.c_str());
-        fflush(stdout);
+    send_hello([this](const std::string &busname) -> int {
+      fprintf(stdout, "Unique bus name: %s\n", busname.c_str());
+      fflush(stdout);
 
-        send_call(
-          DBusMessageBody::mk0(),
-          _s(objectpath_),
-          _s("org.freedesktop.DBus.Introspectable"),
-          _s(destination_),
-          _s("Introspect"),
-          [this](const DBusMessage& message, bool) -> int {
-            print_dbus_message(STDOUT_FILENO, message);
-            return -1;
-          }
-        );
+      send_call(DBusMessageBody::mk0(), _s(objectpath_),
+                _s("org.freedesktop.DBus.Introspectable"), _s(destination_),
+                _s("Introspect"),
+                [](const DBusMessage &message, bool) -> int {
+                  print_dbus_message(STDOUT_FILENO, message);
+                  return -1;
+                });
 
-        return 0;
-      }
-    );
+      return 0;
+    });
   }
 
   // We don't expect to receive any calls.
-  void receive_call(const DBusMessage&) override final {
+  void receive_call(const DBusMessage &) override final {
     logerror("Unexpected incoming call.");
   }
 
   // We don't expect to receive any calls.
-  void receive_signal(const DBusMessage&) override final {
+  void receive_signal(const DBusMessage &) override final {
     logerror("Received a signal.");
   }
 
-  void receive_error(const DBusMessage&) override final {
+  void receive_error(const DBusMessage &) override final {
     logerror("Received an error.");
   }
 
   void disconnect() noexcept override final {}
 
-  void logerror(const char* errmsg) noexcept override final {
+  void logerror(const char *errmsg) noexcept override final {
     fprintf(stderr, "%s\n", errmsg);
   }
 };
 
-void run(
-  const uid_t uid,
-  const char* socketpath,
-  const char* destination,
-  const char* objectpath
-) {
+void run(const uid_t uid, const char *socketpath, const char *destination,
+         const char *objectpath) {
   EPollLoop loop;
-  DBusAuthHandler* handler =
-    new DBusAuthHandler(
-      loop, uid,
-      new IntrospectHandler(socketpath, destination, objectpath)
-    );
+  DBusAuthHandler *handler = new DBusAuthHandler(
+      uid, new IntrospectHandler(socketpath, destination, objectpath));
   if (loop.add_handler(handler) < 0) {
     throw Error(_s("Failed to add IntrospectHandler"));
   }
@@ -97,32 +80,31 @@ void run(
   loop.run();
 }
 
-int main(int argc, char* argv[]) {
-  const char* progname = argc > 0 ? argv[0] : "a.out";
+int main(int argc, char *argv[]) {
+  const char *progname = argc > 0 ? argv[0] : "a.out";
   if (argc < 4) {
     fprintf(
-      stderr,
-      "usage:   %s <unix socket path> <destination> <object path>\n"
-      "example: %s /var/run/dbus/system_bus_socket org.freedesktop.PolicyKit1 /org/freedesktop/PolicyKit1/Authority\n",
-      progname,
-      progname
-    );
+        stderr,
+        "usage:   %s <unix socket path> <destination> <object path>\n"
+        "example: %s /var/run/dbus/system_bus_socket "
+        "org.freedesktop.PolicyKit1 /org/freedesktop/PolicyKit1/Authority\n",
+        progname, progname);
     return 1;
   }
 
   uid_t uid = getuid();
-  const char* socketpath = argv[1];
-  const char* destination = argv[2];
-  const char* objectpath = argv[3];
+  const char *socketpath = argv[1];
+  const char *destination = argv[2];
+  const char *objectpath = argv[3];
 
   try {
     run(uid, socketpath, destination, objectpath);
     return EXIT_SUCCESS;
-  } catch (ErrorWithErrno& e) {
+  } catch (ErrorWithErrno &e) {
     const int err = e.getErrno();
     fprintf(stderr, "%s\n%s\n", e.what(), strerror(err));
     return EXIT_FAILURE;
-  } catch (std::exception& e) {
+  } catch (std::exception &e) {
     fprintf(stderr, "%s\n", e.what());
     return EXIT_FAILURE;
   }
